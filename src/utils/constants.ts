4b76a8a6e9216c7ad3b85c99cb9a65c5a88ef2b1
hadi-makki <165915385+hadi-makki@users.noreply.github.com>
2025-05-03 16:04:28 +0300

    fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { CookieOptions } from 'express';
 export const cookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: false,
+  secure: true,
   sameSite: 'none',
   path: '/',
   maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
