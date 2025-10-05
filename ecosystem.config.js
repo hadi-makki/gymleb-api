0299d2ae398befb859495d6a1d89214c2efadb44
hadi-makki <165915385+hadi-makki@users.noreply.github.com>
2025-10-05 11:04:37 +0300

    fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'gymleb-api',
-      script: 'dist/main.js', // Run the built NestJS project (not src/index.js!)
+      script: 'dist/src/main.js', // Run the built NestJS project (not src/index.js!)
       instances: 1, // Or use "max" for cluster mode
       autorestart: true,
       watch: false, // In production, no need to watch file changes
