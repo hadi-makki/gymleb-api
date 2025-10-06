3702852f170cb31745284ed61b73f13f85d3bb63
hadi-makki <165915385+hadi-makki@users.noreply.github.com>
2025-10-06 09:48:57 +0300

    fixed

--- src/decorators/get-device-id.decorator.ts
+++ src/decorators/get-device-id.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { CookieNames } from 'src/utils/constants';
 
 export const GetDeviceId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.cookies['deviceId'];
+    return request.cookies[CookieNames.DeviceId];
   },
 );
